6198a5d5364a89e2c3a1bc03e66ba624d891fab0
Eduardo Pacheco <edyson51@hotmail.com>
2021-08-31 13:52:02 -0500

    Level 3 exercises

---
+++ level_1/exercises/testing_go/simple_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func IntMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func TestIntMinBasic(t *testing.T) {
+	ans := IntMin(10, 30)
+	if ans != 10 {
+		t.Errorf("IntMin(10, 30) = %d; want 10", ans)
+	}
+}
+
+func TestIntMinTableDriven(t *testing.T) {
+	var tests = []struct {
+		a, b int
+		want int
+	}{
+		{0, 1, 0},
+		{1, 0, 0},
+		{2, -2, -2},
+		{0, -1, -1},
+		{-1, 0, -1},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%d,%d", tt.a, tt.b)
+		t.Run(testname, func(t *testing.T) {
+			ans := IntMin(tt.a, tt.b)
+			if ans != tt.want {
+				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
+	}
+}
+
+func TestSomething(t *testing.T) {
+
+	// assert equality
+	assert.Equal(t, 123, 123, "they should be equal")
+
+	// assert inequality
+	assert.NotEqual(t, 123, 456, "they should not be equal")
+
+	assert.True(t, true, "it's true")
+}
